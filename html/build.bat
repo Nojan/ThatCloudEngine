fdc4fa343cacdec751c59c80e77da6c2785f958e
Mathias Labeyrie <mathias.labeyrie@gmail.com>
2019-10-22 21:46:11 -0400

    EMscripten: test that cmake is in PATH

--- html/build.bat
+++ html/build.bat
@@ -1,3 +1,9 @@
+where /q cmake
+if ERRORLEVEL 1 (
+    echo CMAKE is missing. Ensure it is placed in your PATH.
+    exit /B
+)
+
 pushd %~dp0
 @rem do not call emsdk_env.bat if it has already been done
 @if "%EM_CONFIG%"=="" call "..\..\emsdk\emsdk_env.bat"
