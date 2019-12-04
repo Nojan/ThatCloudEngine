26c26322ae281b1132eb31351056ff0e0ce713b4
Mathias Labeyrie <mathias.labeyrie@gmail.com>
2019-12-03 21:29:30 -0500

    fix tabs

--- sources/opengl_helpers.cpp
+++ sources/opengl_helpers.cpp
@@ -9,18 +9,18 @@ const char* gl_error_enum_string(GLenum e)
 {
     switch(e)
     {
-        case GL_NO_ERROR:
-            return "GL_NO_ERROR";
-	case GL_INVALID_ENUM:
-	    return "GL_INVALID_ENUM";
-	case GL_INVALID_VALUE:
-	    return "GL_INVALID_VALUE";
-	case GL_INVALID_OPERATION:
-	    return "GL_INVALID_OPERATION";
-	case GL_OUT_OF_MEMORY:
-	    return "GL_OUT_OF_MEMORY";
-	default:
-	    return "UNKNOWN ENUM";
+    case GL_NO_ERROR:
+        return "GL_NO_ERROR";
+    case GL_INVALID_ENUM:
+        return "GL_INVALID_ENUM";
+    case GL_INVALID_VALUE:
+        return "GL_INVALID_VALUE";
+    case GL_INVALID_OPERATION:
+        return "GL_INVALID_OPERATION";
+    case GL_OUT_OF_MEMORY:
+        return "GL_OUT_OF_MEMORY";
+    default:
+        return "UNKNOWN ENUM";
     }
     return nullptr;
 }
