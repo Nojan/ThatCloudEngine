5b937ee45083548a8af942eed4db2f6dc20128c6
Mathias Labeyrie <mathias.labeyrie@gmail.com>
2019-12-22 10:56:27 -0500

    Emscripten env now properly provide make

--- html/build.bat
+++ html/build.bat
@@ -8,7 +8,7 @@ pushd %~dp0
 @rem do not call emsdk_env.bat if it has already been done
 @if "%EM_CONFIG%"=="" call "..\..\emsdk\emsdk_env.bat"
 
-set MAKE="%EMSDK%/mingw/7.1.0_64bit/bin/mingw32-make.exe"
+set MAKE="make"
 
 call emcmake cmake -DCMAKE_BUILD_TYPE=Release -D"CMAKE_MAKE_PROGRAM:PATH=%MAKE%" .. -G "MinGW Makefiles"
 call emmake %MAKE%
